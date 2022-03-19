1ca07331e6bc1907da8ae5a5e920473d7b3a9a30
Rodrigo Girão Serrão <5621605+RodrigoGiraoSerrao@users.noreply.github.com>
2022-03-19 14:20:34 +0000

    Replace commands snappify and carbon with create.

--- sniped/__main__.py
+++ sniped/__main__.py
@@ -5,6 +5,7 @@ import json
 import pathlib
 from urllib.request import Request, urlopen
 import sys
+from typing import Optional
 
 import typer
 
@@ -18,8 +19,14 @@ class Service(enum.Enum):
     SNAPPIFY = "snappify"
 
 
-CARBON_ENDPOINT = "https://carbonara-42.herokuapp.com/api/cook"
-SNAPPIFY_ENDPOINT = "https://api.snappify.io/snap/simple"
+ENDPOINTS = {
+    Service.CARBON: "https://carbonara-42.herokuapp.com/api/cook",
+    Service.SNAPPIFY: "https://api.snappify.io/snap/simple",
+}
+CONFIGS = {
+    Service.CARBON: CARBON,
+    Service.SNAPPIFY: SNAPPIFY,
+}
 
 
 def read_code_from_stdin():
@@ -64,7 +71,7 @@ def create(
     service: Service = typer.Argument(..., case_sensitive=False),
     code: str = typer.Argument(
         ...,
-        metavar="code",
+        metavar="CODE_OR_PATH",
         help=(
             "Code to include in the image or path to code file."
             + " Use '-' to read from stdin."
@@ -72,46 +79,50 @@ def create(
     ),
     language: str = typer.Option(
         "auto",
-        metavar="lang",
+        metavar="LANG",
         help="Language for syntax highlighting; 'auto' only works for carbon.",
     ),
     key: str = typer.Option(
-        "api.key",
-        metavar="key_or_path",
+        "",
+        metavar="KEY_OR_PATH",
         help="(Path to file with) API key for Snappify.",
+        envvar="SNAPPIFY_API_KEY",
     ),
-    out: str = typer.Option("out.png", metavar="path"),
+    out: Optional[pathlib.Path] = typer.Option(
+        None,
+        metavar="PATH",
+        help="Write to given file instead of stdout.",
+        dir_okay=False,
+        writable=True,
+    ),
 ):
-    pass
+    """Create a beautiful image from a snippet of code."""
 
+    request = Request(ENDPOINTS[service])
+    request.add_header("Content-Type", "application/json")
 
-@app.command()
-def carbon(
-    code: str,
-    language: str = typer.Argument("auto"),
-    out: str = typer.Argument("out.png"),
-):
-    make_image_from_request(
-        code,
-        language,
-        out,
-        Request(CARBON_ENDPOINT),
-        CARBON,
-    )
+    if service == Service.SNAPPIFY:
+        # Abort if we don't have a valid API key.
+        if not key:
+            typer.echo("Option '--key' required for the Snappify service.", err=True)
+            raise typer.Abort()
+        key = try_reading_from_file(key)
+        request.add_header("Authorization", key)
 
+    code = read_code_from_stdin() if code == "-" else try_reading_from_file(code)
 
-@app.command()
-def snappify(code: str, key: str, language: str, out: str = typer.Argument("out.png")):
-    req = Request(SNAPPIFY_ENDPOINT)
-    key = try_reading_from_file(key)
-    req.add_header("Authorization", key)
-    make_image_from_request(
-        code,
-        language,
-        out,
-        Request(SNAPPIFY_ENDPOINT),
-        SNAPPIFY,
-    )
+    config = CONFIGS[service]
+    config["code"] = code
+    config["language"] = language
+
+    with urlopen(request, data=json.dumps(config).encode()) as response:
+        image = response.read()
+
+    if out is None:
+        sys.stdout.buffer.write(image)
+    else:
+        with open(out, "wb") as f:
+            f.write(image)
 
 
 if __name__ == "__main__":
